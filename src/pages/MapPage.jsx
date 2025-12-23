075cc5d5c1ac5d9566aa0ba33b632fa745b61b7e
Aryan <aryan.az1608@gmail.com>
2025-12-24 00:56:07 +0530

    Refactor Leaflet icon setup to MapPage component
    Moved Leaflet icon configuration from useEffect in ClientOnly to MapPage component.

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -57,17 +57,7 @@ import Sundarbans from "../assets/mapImages/Sundarbans.jpeg";
 import KothapatnamBeach from "../assets/mapImages/KothapatnamBeach.jpg";
 
 /* ===================== LEAFLET MARKER FIX ===================== */
-useEffect(() => {
-  delete L.Icon.Default.prototype._getIconUrl;
-  L.Icon.Default.mergeOptions({
-    iconRetinaUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-    iconUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-    shadowUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-  });
-}, []);
+
 
 
 const ClientOnly = ({ children }) => {
@@ -83,6 +73,18 @@ const ClientOnly = ({ children }) => {
 
 const MapPage = () => {
   const mapRef = useRef(null);
+
+  useEffect(() => {
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+      iconUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+      shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+    });
+  }, []);
 
   const [mapCenter, setMapCenter] = useState([20.5937, 78.9629]);
 
@@ -1125,7 +1127,6 @@ const MapPage = () => {
 
       {/* Map */}
       <div className="flex-1 relative">
-        {mapRef && (
           <MapContainer
               center={mapCenter}
               zoom={5}
